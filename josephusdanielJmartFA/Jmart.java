0632f521ae10dbd331d98b85755d3159d96f9d06
Daniel Andrew <danielandrewtt@gmail.com>
2021-10-06 14:32:35 +0700

    Migrasi ke Eclipse IDE

--- josephusdanielJmartFA/Jmart.java
+++ josephusdanielJmartFA/Jmart.java
@@ -1,15 +1,9 @@
 package josephusdanielJmartFA;
 
-
-/**
- * Write a description of class Jmart here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Jmart
 {
     // instance variables - replace the example below with your own
     public static void main(String[] args) {
+    	System.out.print("Hello from Eclipse");
     }
 }
