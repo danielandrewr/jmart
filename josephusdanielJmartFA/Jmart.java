6586a9b10f433c8f29f27c7de21095758f35f1a9
Daniel Andrew <danielandrewtt@gmail.com>
2021-10-09 11:24:45 +0700

    CS Modul 5 - Hapus message dari migrasi

--- josephusdanielJmartFA/Jmart.java
+++ josephusdanielJmartFA/Jmart.java
@@ -3,6 +3,5 @@ package josephusdanielJmartFA;
 public class Jmart {
     // instance variables - replace the example below with your own
     public static void main(String[] args) {
-    	System.out.print("Hello from Eclipse");
     }
 }
